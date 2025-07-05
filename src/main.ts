ac05f760d3cf1b1338a11be1c532650ed6ddb738
jhoaking <peredoroj@gmail.com>
2025-07-04 22:51:45 -0400

    first commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import './style.css'
+//import './clean-code/06-classes-c'
+//import './solid/01-srp'
+//import './solid/02-open-close-a'
+//import './solid/03-liskov-a'
+import './solid/05-dependency-a'
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>CleanCode y SOLID</h1>
+  <span>Revisar la consola de JavaScript</span>
+`
+
