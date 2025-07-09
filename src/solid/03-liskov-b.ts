b909c06988ba5d0ed4bef88d20e4c5c53eab59d1
jhoaking <peredoroj@gmail.com>
2025-07-09 09:43:49 -0400

    proncipio de liskov

---
+++ src/solid/03-liskov-b.ts
@@ -0,0 +1,53 @@
+export abstract class Vehicle {
+  abstract getNumberOfSeats(): Number;
+}
+
+export class Tesla extends Vehicle {
+  constructor(private numberOfSeats: number) {
+    super();
+  }
+
+  getNumberOfSeats() {
+    return this.numberOfSeats;
+  }
+}
+
+export class Audi extends Vehicle {
+  constructor(private numberOfSeats: number) {
+    super();
+  }
+
+  getNumberOfSeats() {
+    return this.numberOfSeats;
+  }
+}
+
+export class Toyota extends Vehicle {
+  constructor(private numberOfSeats: number) {
+    super();
+  }
+
+  getNumberOfSeats() {
+    return this.numberOfSeats;
+  }
+}
+
+export class Honda extends Vehicle {
+  constructor(private numberOfSeats: number) {
+    super();
+  }
+
+  getNumberOfSeats() {
+    return this.numberOfSeats;
+  }
+}
+
+export class Volvo extends Vehicle {
+  constructor(private numberOfSeats: number) {
+    super();
+  }
+
+  getNumberOfSeats() {
+    return this.numberOfSeats;
+  }
+}
